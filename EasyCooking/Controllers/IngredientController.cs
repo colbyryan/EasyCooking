33c5a3eaa9c1e2b2a8369a1c16fa43080c86f656
Colby Ryan <colbyr.ryan@gmail.com>
2022-01-31 16:01:43 -0600

    still working on ticket 13

---
+++ EasyCooking/Controllers/IngredientController.cs
@@ -0,0 +1,96 @@
+﻿using EasyCooking.Repositories;
+using EasyCooking.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyCooking.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private readonly IIngredientRepository _ingredientRepository;
+        public IngredientController(IIngredientRepository ingredientRepository)
+        // GET: RecipeController
+        {
+            _ingredientRepository = ingredientRepository;
+        }
+        // GET: IngredientController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: IngredientController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: IngredientController/Create
+        public ActionResult Create(Ingredient ingredient)
+        {
+            _ingredientRepository.Add(ingredient);
+            return RedirectToAction("Details", "Recipe");
+        }
+
+        // POST: IngredientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IngredientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: IngredientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: IngredientController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: IngredientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
