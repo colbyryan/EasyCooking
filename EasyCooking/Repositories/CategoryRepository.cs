34c076b8413bb009fbb68b862a96e59a487c3e2e
Colby Ryan <colbyr.ryan@gmail.com>
2022-01-31 20:00:17 -0600

    Started working on category

--- EasyCooking/Repositories/CategoryRepository.cs
+++ EasyCooking/Repositories/CategoryRepository.cs
@@ -54,5 +54,86 @@ namespace EasyCooking.Repositories
                     }
                 }
             }
+        public void CreateCategory(Category category)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Category (
+                            Name )
+                        OUTPUT INSERTED.ID
+                        VALUES (
+                            @Name)";
+                    cmd.Parameters.AddWithValue("@Name", category.Name);
+                    category.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name
+                                        FROM Category
+                                        WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    Category category = null;
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (category == null)
+                        {
+                            category = new Category()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+                        }
+                    }
+                    reader.Close();
+                    return category;
+                }
+            }
+        }
+
+        public void Update(Category category)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = @"UPDATE Category
+                                        SET Name = @name
+                                        WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@name", category.Name);
+                    cmd.Parameters.AddWithValue("@id", category.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Category WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
--- EasyCooking/Repositories/ICategoryRepository.cs
+++ EasyCooking/Repositories/ICategoryRepository.cs
@@ -5,6 +5,10 @@ namespace EasyCooking.Repositories
 {
     public interface ICategoryRepository
     {
+        void CreateCategory(Category category);
+        void Delete(int id);
         List<Category> GetAll();
+        Category GetCategoryById(int id);
+        void Update(Category category);
     }
 }
